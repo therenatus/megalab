112f8df80df948af5bdaa5e2e599ed3e400e16d9
rinadmoly <rinadmoly@gmail.com>
2022-08-19 00:12:22 +0600

    merge and updae task2

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let day = document.getElementById('day');
 let time = document.getElementById('time');
-let data = new Date();
-let dayData = data.toLocaleString('en', {weekday: 'long'});
-let timeData = data.toLocaleString('en', {hour: '2-digit', minute: 'numeric', second:'numeric'});
 
-window.addEventListener('load', function() {
-    day.innerHTML = dayData;
-    time.innerHTML = timeData
+window.addEventListener('DOMContentLoaded', function() {
+    toHtml();
 })
+
+function toHtml () {
+    let data = new Date();
+    let dayData = data.toLocaleString('en', {weekday: 'long'});
+    let timeData = data.toLocaleString('en', {hour: '2-digit', minute: 'numeric', second:'numeric'});
+    day.innerHTML = dayData;
+    time.innerHTML = timeData;
+    setTimeout(()=> {
+        toHtml();
+    }, 1000)
+}
