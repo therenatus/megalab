cd056eac7752879a7d7a4e30ac1d1af5b8d3df46
rinadmoly <rinadmoly@gmail.com>
2022-08-20 22:32:59 +0600

    merge and update task8

--- task8/src/App.js
+++ task8/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './index.module.scss';
 
 function App() {
 	const [number, setNumber] = useState(0);
+	const [disabled, setDisabled] = useState(false);
+
+	useEffect(() => {
+		let autoDec = null
+		if(!number){
+			setDisabled(false)
+		}
+		if(setDisabled){
+		autoDec = setInterval(()=>{
+			if(number && number< 1000){
+				setNumber(number-1)
+			}
+		},1000)
+	  }
+	  return ()=> clearInterval(autoDec)
+	},  [number])
+
 	const changeNumber = (action) => {
 		switch(action) {
 			case 'increase':
@@ -20,9 +37,14 @@ function App() {
 		<div className={styles.main}>
 			<div className={styles.number}>{number}</div>
 			<div className={styles.buttonGroup}>
-				<button onClick={() => changeNumber('increase')}>Increase</button>
-				<button onClick={() => changeNumber('decrease')}>Decrease</button>
-				<button onClick={() => changeNumber('reset')}>Reset</button>
+				<button disabled={disabled} onClick={() => changeNumber('increase')}>Increase</button>
+				<button disabled={disabled} onClick={() => changeNumber('decrease')}>Decrease</button>
+				<button disabled={disabled} onClick={() => changeNumber('reset')}>Reset</button>
+				<input type='number' value={number}
+					onChange={e=> {
+					setNumber(+e.target.value > 1000 ? number : e.target.value)
+					setDisabled(true)
+					}} placeholder='введите число'/>
 			</div>
 		</div>
 	);
